1b5ef5c6254b0903e88293a6c926592e3087621e
Federico Aloi <federico.aloi@gmail.com>
2018-09-06 21:29:26 -0300

    Mejorado el código de valorPara

--- app/conversorArabigoRomano.js
+++ app/conversorArabigoRomano.js
@@ -6,18 +6,14 @@ class ConversorArabigoRomano {
 
     if (numeroArabigo === 4) {
       resultado = "IV";
-    } else if (numeroArabigo === 5) {
+      numeroArabigo -= 4;
+    } else if (numeroArabigo >= 5) {
       resultado = "V";
-    } else if (numeroArabigo === 6) {
-      resultado = "VI";
-    } else if (numeroArabigo === 7) {
-      resultado = "VII";
-    } else if (numeroArabigo === 8) {
-      resultado = "VIII";      
-    } else {
-      for (var i = 0; i < numeroArabigo; i++) {
-        resultado += "I"
-      }
+      numeroArabigo -= 5;
+    }
+
+    for (var i = 0; i < numeroArabigo; i++) {
+      resultado += "I"
     }
 
     return resultado;
